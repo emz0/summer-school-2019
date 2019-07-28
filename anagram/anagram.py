aabc478e5c36b31908a8ae11b2b5c06d2288c974
Martin Žalondek <martin.zalondek@exponea.com>
2019-07-28 22:51:33 +0200

    Add anagram concept

---
+++ anagram/anagram.py
@@ -0,0 +1,9 @@
+# Read words.txt and store its content into a list
+
+while True:
+    # Read user input
+    # Check if you could build another word out of letters in the user input
+    # If yes print out all the words
+    pass
+
+
