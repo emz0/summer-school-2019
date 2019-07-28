b3829ac8396556749e6ecf149662465b67306641
Martin Žalondek <martin.zalondek@exponea.com>
2019-07-28 22:36:06 +0200

    Add pizza concept

---
+++ pizza/pizza.py
@@ -0,0 +1,32 @@
+import csv
+import time
+
+
+def format_pizza_menu(pizza_menu):
+    # prepare the pizza menu so it can be printed out nicely :-)
+    pass
+
+
+def format_basket(basket):
+    # prepare the basket so it can be printed out nicely :-)
+    pass
+
+def print_output(output):
+    pass
+
+# read pizza menu from file
+
+# print pizza menu
+
+while True:
+    # Read user input until you get letter 'c' - which means he wants to check out
+
+    # Check if user chose pizza that is in the menu and if it is add it to the basket
+    # If it is not let him know and choose again
+
+    # Print content of the basket and the total cost
+    # Print the pizza menu
+    pass
+
+# Export content of the basket into file
+# Try to make the name of the file unique
